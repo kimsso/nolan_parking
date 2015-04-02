cab1f8348bd4d3149775b4d6d168846ea0f39f91
woosung <loftyws91@naver.com>
2015-04-02 20:35:53 +0900

    woosung

---
+++ parking/Assets/Map_Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map_Rotate : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.RotateAround ( new Vector3( 0, 0, 0 ) , new Vector3( 0, 1, 0 ), 1 ); 
+	}
+}
