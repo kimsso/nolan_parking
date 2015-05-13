a98841a9382c98d1da34b891876a87b476b7524a
woosung <loftyws91@naver.com>
2015-05-13 20:40:57 +0900

    update

--- parking/Assets/Script/Car_Moving.cs
+++ parking/Assets/Script/Car_Moving.cs
@@ -75,14 +75,9 @@ public class Car_Moving : MonoBehaviour {
 			this.transform.position += this.transform.forward * speed * Time.deltaTime;
 
 			main_cam.transform.position = Vector3.Lerp(main_cam.transform.position,car_cam.transform.position,0.05f);
-<<<<<<< HEAD
-			//main_cam.transform.rotation = Quaternion.Lerp(main_cam.transform.rotation,car_cam.transform.rotation,0.05f);
-			
-=======
+
 			main_cam.transform.rotation = Quaternion.Lerp(main_cam.transform.rotation,car_cam.transform.rotation,0.05f);
 
-
->>>>>>> 6ee982fd0e0b98b31c32f5a7614119883162a3ff
 			/** speed up/down **/
 			if(Input.GetAxis("Vertical")>0)
 			{
