e0831217829fd2f68793e2f40f94b6518b53a1a2
sodam <kimsd16@naver.com>
2015-05-19 17:12:19 +0900

    rear mirror merge

---
+++ parking/Assets/Script/Car_Handle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car_Handle : MonoBehaviour {
+
+	public GameObject car;
+	
+	public float turn;
+	float pi;
+
+	// Use this for initialization
+	void Start () {
+		car = GameObject.Find("Car");
+		//pi=3.141592;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		turn = car.GetComponent<Car_Moving>().turning;
+
+		this.transform.Rotate (0, 0, -turn*10);
+		//this.transform.RotateAround(center.transform.position,
+		  //                          new Vector3(-1.0f, 0.0f, 0.0f), turn*10);
+	}
+}
---
+++ parking/Assets/Script/Mode_Select.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mode_Select : MonoBehaviour {
+
+	public int mode;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		if( GUI.Button(new Rect(Screen.width/2-65, Screen.height/2+10, 60, 25), "전진주차") == true ){
+		    Application.LoadLevel("SelectMap_Scene");		
+			mode = 1;
+		}
+
+		if( GUI.Button(new Rect(Screen.width/2, Screen.height/2+10, 60, 25), "후진주차") == true ){
+			Application.LoadLevel("SelectMap_Scene");
+			mode = 2;
+		}
+
+		if( GUI.Button(new Rect(Screen.width/2+65, Screen.height/2+10, 60, 25), "평행주차") == true ){
+			Application.LoadLevel("SelectMap_Scene");
+			mode = 3;
+		}
+	}
+}
