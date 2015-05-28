121b82d6ca345fcf1a4d45f4c363608afd8bbecb
woosung <loftyws91@naver.com>
2015-05-28 09:04:01 +0900

    game_passfail update

--- parking/Assets/Script/Game_PassFail.cs
+++ parking/Assets/Script/Game_PassFail.cs
@@ -10,9 +10,6 @@ public class Game_PassFail : MonoBehaviour {
 	public GameObject back_left;
 	public GameObject back_right;
 	
-	public GameObject front_collider;
-	public GameObject back_collider;
-	
 	Vector3 spot_pos;
 	Vector3 spot_scale;
 	
@@ -20,9 +17,6 @@ public class Game_PassFail : MonoBehaviour {
 	Vector3 frontRight_pos;
 	Vector3 backLeft_pos;
 	Vector3 backRight_pos;
-	
-	Vector3 frontCol_pos;
-	Vector3 backCol_pos;
 	
 	public bool success_flag;
 	
@@ -32,6 +26,8 @@ public class Game_PassFail : MonoBehaviour {
 	public bool backRight_flag;
 	
 	public bool mode_flag;
+	
+	public int wheel_cnt;
 	
 	// Use this for initialization
 	void Start () {
@@ -50,18 +46,17 @@ public class Game_PassFail : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
 		frontLeft_pos=front_left.GetComponent<Transform>().position;
 		frontRight_pos=front_right.GetComponent<Transform>().position;
 		backLeft_pos=back_left.GetComponent<Transform>().position;
 		backRight_pos=back_right.GetComponent<Transform>().position;
 		
-		frontCol_pos=front_collider.GetComponent<Transform>().position;
-		backCol_pos=back_collider.GetComponent<Transform>().position;
 	}
-	
+	/*
 	void OnTriggerEnter(Collider col){
 		if(Mode_Select.modeNum == 1){
-			if(col.name == "back_collider")
+			if(col.name == "front_collider")
 				mode_flag=true;
 			else
 				mode_flag=false;
@@ -69,7 +64,7 @@ public class Game_PassFail : MonoBehaviour {
 		}
 		
 		if(Mode_Select.modeNum == 2){
-			if(col.name == "front_collider")
+			if(col.name == "back_collider")
 				mode_flag=true;
 			else
 				mode_flag=false;
@@ -79,15 +74,44 @@ public class Game_PassFail : MonoBehaviour {
 		if(Mode_Select.modeNum == 3){
 			mode_flag=true;
 		}
-	}
+	}*/
 	
 	void OnTriggerStay(Collider col){
+		wheel_cnt=0;
+		
 		frontLeft_flag=ParkingPass(frontLeft_pos);
 		frontRight_flag=ParkingPass(frontRight_pos);
 		backLeft_flag=ParkingPass(backLeft_pos);
 		backRight_flag=ParkingPass(backRight_pos);
 		
-		if(
+		if(frontLeft_flag)
+			wheel_cnt++;
+		if(frontRight_flag)
+			wheel_cnt++;
+		if(backLeft_flag)
+			wheel_cnt++;
+		if(backRight_flag)
+			wheel_cnt++;
+		
+		if(wheel_cnt == 2){
+			if(Mode_Select.modeNum == 1){
+				if(frontLeft_flag && frontRight_flag)
+					mode_flag=true;
+				else
+					mode_flag=false;
+			}
+			else if(Mode_Select.modeNum == 2){
+				if(backLeft_flag && backRight_flag)
+					mode_flag=true;
+				else
+					mode_flag=false;
+			}
+			else if(Mode_Select.modeNum == 3){
+				mode_flag=true;
+			}
+		}
+		
+		if(mode_flag &&
 		   frontLeft_flag && frontRight_flag && backLeft_flag && backRight_flag)
 			success_flag=true;
 		else
